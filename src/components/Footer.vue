f1576d84d3aa8d16745318f37e54f0e0ccf5680d
Budrewicz, Mateusz <mateusz.budrewicz@atos.net>
2018-10-10 11:08:46 +0200

    Add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <div class="col-md-12 fixed-bottom text-center">
+            <p>Mateusz Budrewicz 2018</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
